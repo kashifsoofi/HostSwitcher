88e13b45890bdf8238ec9b2d27a3f9cdaba9ec5c
Kashif Jamal Soofi <kashif.soofi+git@gmail.com>
2013-04-04 23:07:50 +0100

    App form with tray icon and menu

---
+++ HostsSwitcher/AppForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace HostsSwitcher
+{
+    partial class AppForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AppForm));
+            this.TrayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.TrayMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TrayMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TrayIcon
+            // 
+            this.TrayIcon.ContextMenuStrip = this.TrayMenu;
+            this.TrayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("TrayIcon.Icon")));
+            this.TrayIcon.Text = "Hosts Switcher Utility";
+            this.TrayIcon.Visible = true;
+            // 
+            // TrayMenu
+            // 
+            this.TrayMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.TrayMenu.Name = "TrayMenu";
+            this.TrayMenu.Size = new System.Drawing.Size(93, 26);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // AppForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "AppForm";
+            this.ShowInTaskbar = false;
+            this.Text = "Hosts Switcher";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.TrayMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon TrayIcon;
+        private System.Windows.Forms.ContextMenuStrip TrayMenu;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
---
+++ HostsSwitcher/AppForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace HostsSwitcher
+{
+    public partial class AppForm : Form
+    {
+        public AppForm()
+        {
+            InitializeComponent();
+
+            string hostsPath = Environment.GetFolderPath(Environment.SpecialFolder.System) + @"\drivers\etc\hosts";
+            // Backup original file if does not exist
+            string originalHostsPath = hostsPath + ".original";
+            if (!File.Exists(originalHostsPath))
+            {
+                File.Copy(hostsPath, originalHostsPath);
+            }
+            string a = string.Empty;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
